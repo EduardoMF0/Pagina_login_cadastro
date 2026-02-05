19ee7f6f8985974eb4842774b4e1a968d89bd57e
EduardoMF0 <159490289+EduardoMF0@users.noreply.github.com>
2026-02-05 16:39:57 -0300

    Update Program.cs

--- ApiCrud/Program.cs
+++ ApiCrud/Program.cs
@@ -4,55 +4,44 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Controllers
+builder.Services.AddControllers();
+
+// Authorization (mesmo sem JWT)
 builder.Services.AddAuthorization();
 
-// Add services to the container.
-
+// DbContext
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("DefaultConnection")
     )
 );
 
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// DI
 builder.Services.AddTransient<IUserRepository, UserRepository>();
 
-
-
+// CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyMethod()
-                  .AllowAnyHeader();
-        });
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
 });
-
-
-
 
 var app = builder.Build();
 
-app.UseCors("AllowAll"); // Ativar CORS
-
-
-// Configure the HTTP request pipeline.
-/*if (app.Environment.IsDevelopment())
-{*/
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-
-app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
