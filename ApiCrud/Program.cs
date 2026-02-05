a9e4bfbbd574f72dcbb0cb610b2c2b0b31af4b62
EduardoMF0 <159490289+EduardoMF0@users.noreply.github.com>
2026-02-05 16:24:04 -0300

    add new version ApiCrud

--- ApiCrud/Program.cs
+++ ApiCrud/Program.cs
@@ -6,9 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DataBase")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
